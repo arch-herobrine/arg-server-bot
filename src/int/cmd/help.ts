368017f1468f8406ff47ebb1b3922f954a485c85
arch-herobrine <112319383+arch-herobrine@users.noreply.github.com>
2026-05-01 20:17:11 +0900

    help command

---
+++ src/int/cmd/help.ts
@@ -0,0 +1,39 @@
+import {
+    ActionRowBuilder,
+    ButtonBuilder,
+    ButtonStyle,
+    ChatInputCommandInteraction,
+    CmdContent,
+    ComponentType
+} from "discord.js";
+import config from "../../config.js";
+
+const data: CmdContent = {
+    "data": {
+        "name": "help",
+        "description": "helpを表示",
+    },
+    async exec(int, client) {
+        if (!int.isChatInputCommand()) return;
+        int.reply({
+            "embeds": [{
+                "title": "使い方とかいろいろ",
+                "color": 0x71f26d,
+                "fields": [
+                    {
+                        "name": "CCB/CC",
+                        "value": "CCB<=(計算式)、CC<=(計算式)とすることでココフォリアみたいにダイスが振れます。後ろに余計な文字列があっても振れます。計算式には整数と\"+\",\"-\",\"\\*\",\"/\",\"()\"が使えます"
+                            +"例: CCB<=10, CCB<=75/2, CCB<=12\\*5, CCB<=50+((16-8)\\*5)",
+                    },
+                    {
+                        "name": "その他ダイス",
+                        "value": "dice (ダイスコード)[=/<>/<=/>=/</>][計算式]とすることでココフォリアみたいにダイスが振れます。後ろに余計な文字列があっても振れます。計算式には整数と\"+\",\"-\",\"\\*\",\"/\",\"()\"が使えます\n"
+                            + "例: dice1d6<>1, dice2d6+5>=10, dice(2d6+1d4)\\*2, dice(1d6-1d4)/2",
+                    }
+                ]
+            }]
+        })
+    }
+}
+
+export default data;
